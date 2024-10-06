16b212debcf3c11237e5eb9b703374194edf3a5a
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-10-06 19:00:40 +0300

    degiştirme işleminin yapılması

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -20,6 +20,14 @@ function App() {
     setTodos([...todo.filter((data) => data.id !== todoIndex)]);
   };
 
+  const handleUpdateTodo = (id: number, newContent: string) => {
+    setTodos(
+      todo.map((todo) =>
+        todo.id === id ? { ...todo, content: newContent } : todo
+      )
+    );
+  };
+
   return (
     <div className="app">
       <h1 style={{ textAlign: "center", color: "#267198", fontSize: "45px" }}>
@@ -27,7 +35,11 @@ function App() {
       </h1>
       <div className="app-container">
         <TodoCreate onCreateTodo={createTodo} />
-        <Todo todoList={todo} onRemoveTodo={removeTodo} />
+        <Todo
+          todoList={todo}
+          onRemoveTodo={removeTodo}
+          onUpdateTodo={handleUpdateTodo}
+        />
       </div>
     </div>
   );
--- todolist/src/components/Todo.tsx
+++ todolist/src/components/Todo.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import { MdOutlineEdit } from "react-icons/md";
+import { FaCheck } from "react-icons/fa";
+
 import "../css/Todo.css";
 
 interface Todo {
@@ -10,18 +12,44 @@ interface Todo {
 
 interface TodoProps {
   todoList: Todo[];
+  onRemoveTodo: (id: number) => void; // onRemoveTodo fonksiyonunu props olarak alıyoruz.
+  onUpdateTodo: (id: number, newContent: string) => void; // onUpdateTodo fonksiyonunu ekliyoruz.
 }
 
-function Todo({ todoList, onRemoveTodo }: TodoProps) {
+function Todo({ todoList, onRemoveTodo, onUpdateTodo }: TodoProps) {
+  const [editableTodoId, setEditableTodoId] = useState<number | null>(null);
+  const [newContent, setNewContent] = useState<string>("");
+
   return (
     <div className="main-container">
       {todoList?.map((todo) => (
         <div className="todo-container" key={todo.id}>
-          <div>{todo.content}</div>
+          {editableTodoId === todo.id ? (
+            <input
+              value={newContent}
+              onChange={(e) => setNewContent(e.target.value)} // Input değeri değiştiğinde newContent'i güncelle
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "10px",
+                borderRadius: "25px",
+                backgroundColor: "#609296",
+                fontSize: "20px",
+                color: "white",
+                fontWeight: "600",
+              }}
+              type="text"
+              placeholder="Yapılacak Görevi değiştirin..."
+            />
+          ) : (
+            <div>{todo.content}</div>
+          )}
+
           <div className="process-container">
             <div className="process">
               <IoMdClose
-                onClick={(event) => onRemoveTodo(todo.id)}
+                onClick={() => onRemoveTodo(todo.id)}
                 style={{
                   borderRadius: "50%",
                   background: "black",
@@ -31,16 +59,38 @@ function Todo({ todoList, onRemoveTodo }: TodoProps) {
                   cursor: "pointer",
                 }}
               />
-              <MdOutlineEdit
-                style={{
-                  borderRadius: "50%",
-                  background: "black",
-                  color: "white",
-                  fontSize: "30px",
-                  padding: "8px",
-                  cursor: "pointer",
-                }}
-              />
+              {editableTodoId === todo.id ? (
+                <FaCheck
+                  onClick={() => {
+                    onUpdateTodo(todo.id, newContent);
+                    setEditableTodoId(null); // Düzenleme modunu kapat
+                    setNewContent("");
+                  }}
+                  style={{
+                    borderRadius: "50%",
+                    background: "black",
+                    color: "white",
+                    fontSize: "30px",
+                    padding: "8px",
+                    cursor: "pointer",
+                  }}
+                />
+              ) : (
+                <MdOutlineEdit
+                  onClick={() => {
+                    setEditableTodoId(todo.id); // Düzenleme moduna geç
+                    setNewContent(todo.content); // Mevcut içeriği inputta göster
+                  }}
+                  style={{
+                    borderRadius: "50%",
+                    background: "black",
+                    color: "white",
+                    fontSize: "30px",
+                    padding: "8px",
+                    cursor: "pointer",
+                  }}
+                />
+              )}
             </div>
           </div>
         </div>
